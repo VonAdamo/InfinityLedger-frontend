e71f1cc016b438a4dd539b03e9895339e49eb432
VonAdamo <148451831+VonAdamo@users.noreply.github.com>
2024-07-10 23:13:30 +0200

    added transaction functionality with endpoints to go with it

--- src/pages/Transactions.jsx
+++ src/pages/Transactions.jsx
@@ -1,7 +1,54 @@
+import React, { useState } from 'react';
+import { sendTransaction, mineBlock, getTransactionPool } from '../services/HttpClient';
+
 export const Transactions = () => {
-    return (
-        <>
-            <h1>Transactions</h1>
-        </>
-    )
-}
+  const [sender, setSender] = useState('');
+  const [recipient, setRecipient] = useState('');
+  const [amount, setAmount] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await sendTransaction(sender, recipient, amount);
+      console.log(response);
+      window.alert('Successfully added transaction. Please mine the block to confirm');
+      setSender('');
+      setRecipient('');
+      setAmount('');
+    } catch (error) {
+      console.error(error);
+      window.alert('Transaction failed. Please try again.');
+    }
+  };
+
+  const handleMineBlock = async () => {
+    try {
+      const response = await mineBlock();
+      window.alert('Block mined successfully and added to the blockchain.');
+    } catch (error) {
+      console.error('Error mining transactions:', error);
+    }
+  };
+
+  return (
+    <div className='sendTransactionWrapper'>
+      <h1>Send a Transaction</h1>
+      <form onSubmit={handleSubmit}>
+        <div className='gap'>
+          <label>Sender:</label>
+          <input type="text" value={sender} onChange={(e) => setSender(e.target.value)} />
+        </div>
+        <div className='gap'>
+          <label>Recipient:</label>
+          <input type="text" value={recipient} onChange={(e) => setRecipient(e.target.value)} />
+        </div>
+        <div className='gap'>
+          <label>Amount:</label>
+          <input type="text" value={amount} onChange={(e) => setAmount(e.target.value)} />
+        </div>
+        <button type="submit">Add Transaction</button>
+        <button onClick={handleMineBlock}>Mine Block</button>
+      </form>
+    </div>
+  );
+};
--- src/services/HttpClient.jsx
+++ src/services/HttpClient.jsx
@@ -5,3 +5,24 @@ export const getBlockchain = async () => {
 
     return response.data;
 };
+
+export const sendTransaction = async (sender, recipient, amount) => {
+    const transactionData = {
+      sender: sender,
+      recipient: recipient,
+      amount: amount
+    };
+  
+    const response = await axios.post('http://localhost:5001/api/v1/wallet/transaction', transactionData);
+    return response.data;
+  };
+  
+  export const getTransactionPool = async () => {
+    const response = await axios.get('http://localhost:5001/api/v1/wallet/transactions');
+    return response.data;
+  };
+  
+  export const mineBlock = async () => {
+    const response = await axios.get('http://localhost:5001/api/v1/wallet/mine');
+    return response.data;
+  };
